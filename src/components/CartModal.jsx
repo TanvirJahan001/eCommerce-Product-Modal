1d19739a660323f3724a13889c23091c46f682e3
Ash Sams Md Tanvir Jahan <129387510+TanvirJahan001@users.noreply.github.com>
2024-12-11 20:48:29 +0600

    Update CartModal.jsx

--- src/components/CartModal.jsx
+++ src/components/CartModal.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const CartModal = ({ cart, onClose, removeItem }) => {
+const CartModal = ({ cart, onClose }) => {
   const totalPrice = cart.reduce(
     (sum, item) => sum + item.price * item.quantity,
     0
@@ -11,7 +11,7 @@ const CartModal = ({ cart, onClose, removeItem }) => {
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-800 bg-opacity-75">
       <div className="bg-white rounded-lg shadow-lg w-full max-w-4xl">
         <div className="bg-white text-slate-700 p-4 flex justify-between items-center rounded-t-lg">
-          <h2 className="text-lg font-medium">Your Cart</h2>
+          <h2 className="text-2xl font-medium">Your Cart</h2>
           <button
             onClick={onClose}
             className="text-slate-500 hover:text-slate-700"
@@ -34,15 +34,14 @@ const CartModal = ({ cart, onClose, removeItem }) => {
         </div>
 
         <div className="p-6">
-          <table className="w-full">
+          <table className="w-full border-collapse">
             <thead>
               <tr className="bg-white text-slate-400 font-normal">
                 <th className="py-2 px-4 text-left">Item</th>
-                <th className="py-2 px-4 text-left">Color</th>
-                <th className="py-2 px-4 text-left">Size</th>
+                <th className="py-2 px-4 text-center">Color</th>
+                <th className="py-2 px-4 text-center">Size</th>
                 <th className="py-2 px-4 text-center">Qty</th>
-                <th className="py-2 px-4 text-right">Price</th>
-                <th className="py-2 px-4 text-center">Actions</th>
+                <th className="py-2 px-4 text-center">Price</th>
               </tr>
             </thead>
             <tbody>
@@ -54,33 +53,30 @@ const CartModal = ({ cart, onClose, removeItem }) => {
                       alt={item.name}
                       className="w-12 h-12 mr-4 rounded"
                     />
-                    <span className="font-medium">{item.name}</span>
+                    <span className="font-medium text-slate-700">
+                      {item.name}
+                    </span>
                   </td>
-                  <td className="py-4 px-4">{item.color}</td>
-                  <td className="py-4 px-4">{item.size}</td>
+                  <td className="py-4 px-4 text-center">{item.color}</td>
+                  <td className="py-4 px-4 text-center">{item.size}</td>
                   <td className="py-4 px-4 text-center">{item.quantity}</td>
                   <td className="py-4 px-4 text-center">
                     ${(item.price * item.quantity).toFixed(2)}
-                  </td>
-                  <td className="py-4 px-4 text-center">
-                    <button
-                      onClick={() => removeItem(item.id)}
-                      className="px-3 py-1 text-red-500 hover:text-red-700"
-                    >
-                      Remove
-                    </button>
                   </td>
                 </tr>
               ))}
             </tbody>
             <tfoot>
               <tr className="border-t border-gray-200">
-                <td colSpan={3} className="py-4"></td>{" "}
+                <td className="text-neutral-700 font-bold py-4 px-4 flex items-center">
+                  Total
+                </td>
+                <td colSpan={2} className="py-3"></td>
                 {/* Empty space for alignment */}
-                <td className="py-4 text-center font-medium text-gray-900">
+                <td className="py-4 text-center font-bold text-slate-700">
                   {totalQty}
                 </td>
-                <td className="py-4 text-right font-medium text-gray-900">
+                <td className="py-4 text-center font-bold text-slate-700">
                   ${totalPrice.toFixed(2)}
                 </td>
                 <td className="py-4"></td> {/* Empty space for alignment */}
@@ -92,7 +88,7 @@ const CartModal = ({ cart, onClose, removeItem }) => {
         <div className="bg-white p-4 flex justify-end rounded-b-lg">
           <button
             onClick={onClose}
-            className="bg-white text-slate-700 border border-zinc-200 px-4 py-2 rounded hover:bg-zinc-200 mr-2"
+            className="bg-white text-slate-700 font-bold border border-zinc-200 px-4 py-2 rounded hover:bg-zinc-200 mr-2"
           >
             Continue Shopping
           </button>
